5b6e62a1ae9685754aa0b9796562dc8a0865896e
Elias Lorandi <elias.lorandi@hotmail.com>
2024-01-25 22:18:50 -0300

    Upload de imagem

--- config/params.php
+++ config/params.php
@@ -4,4 +4,8 @@ return [
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
+    'uploads' => [
+        'path' => '@app/web/uploads/ofertas',
+        //'url' => '/uploads',
+    ],
 ];
